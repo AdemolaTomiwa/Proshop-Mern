6e843664d0b7617979f85a106378841fdaa92137
thetommedia@gmail.com <thetommedia@gmail.com>
2021-12-01 23:52:47 +0100

    Initial commit

---
+++ frontend/src/products.js
@@ -0,0 +1,69 @@
+const products = [
+   {
+      _id: '1',
+      name: 'Airpods wireless Bluetooth',
+      image: '/images/airpods.jpg',
+      description:
+         'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+      brand: 'Apple',
+      category: 'Electronics',
+      price: 89.99,
+      countInStock: 3,
+      rating: 4.5,
+      numReviews: 4,
+   },
+   {
+      _id: '2',
+      name: 'Amazon Echo Dot 3rd Generation',
+      image: '/images/alexa.jpg',
+      description:
+         'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+      brand: 'Amazon',
+      category: 'Electronics',
+      price: 89.99,
+      countInStock: 6,
+      rating: 3,
+      numReviews: 2,
+   },
+   {
+      _id: '3',
+      name: 'Cannon EOS 80D DSLR Camera',
+      image: '/images/camera.jpg',
+      description:
+         'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+      brand: 'Nikon',
+      category: 'Electronics',
+      price: 876.99,
+      countInStock: 7,
+      rating: 4,
+      numReviews: 4,
+   },
+   {
+      _id: '4',
+      name: 'Sony Playstation 4 Pro White Version',
+      image: '/images/playstation.jpg',
+      description:
+         'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+      brand: 'Sony',
+      category: 'Electronics',
+      price: 559.99,
+      countInStock: 10,
+      rating: 5,
+      numReviews: 5,
+   },
+   {
+      _id: '6',
+      name: 'iPhone 11 Pro 256GB Memory',
+      image: '/images/phone.jpg',
+      description:
+         'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      brand: 'iPhone',
+      category: 'Electronics',
+      price: 349.99,
+      countInStock: 3,
+      rating: 4.5,
+      numReviews: 4,
+   },
+];
+
+export default products;
